76d373929976df000a8568fd5953c9487a1ec37b
imhanseong <hans_hanseong@dsm.hs.kr>
2025-07-14 23:51:55 +0900

    feat :: UserService 추가

---
+++ src/main/kotlin/team/twelve/ahu/domain/auth/service/UserService.kt
@@ -0,0 +1,19 @@
+package team.twelve.ahu.domain.auth.service
+
+import org.springframework.stereotype.Service
+import team.twelve.ahu.domain.auth.entity.User
+import team.twelve.ahu.domain.auth.presentation.repository.UserRepository
+import java.util.*
+
+@Service
+class UserService (
+    private val userRepository: UserRepository
+){
+    fun findOrCreateUser(email: String, name: String): User {
+        return userRepository.findByEmail(email)
+            ?: userRepository.save(User(email = email, name = name))
+    }
+
+    fun findById(id : UUID): User =
+        userRepository.findById(id).orElseThrow {NoSuchElementException("User not found")}
+}
