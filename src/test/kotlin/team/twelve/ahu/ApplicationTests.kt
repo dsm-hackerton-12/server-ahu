189532401c7710f8e8fb9f32844703a6d5a738f3
kangeunchan <kangeunchan080310@gmail.com>
2025-07-14 19:01:25 +0900

    feat : Initialize Spring Boot project. Configure Kotlin, Gradle, and essential settings. Add starter files, dependencies, and Gradle wrapper.

---
+++ src/test/kotlin/team/twelve/ahu/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.twelve.ahu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
