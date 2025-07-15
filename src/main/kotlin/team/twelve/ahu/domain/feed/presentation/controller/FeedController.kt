b4c509bed5c407f0f910067ceab829d7224a3727
kangeunchan <kangeunchan080310@gmail.com>
2025-07-15 09:00:04 +0900

    feat : implement retrieve all feeds API with service, DTO, and repository integration

--- src/main/kotlin/team/twelve/ahu/domain/feed/presentation/controller/FeedController.kt
+++ src/main/kotlin/team/twelve/ahu/domain/feed/presentation/controller/FeedController.kt
@@ -1,6 +1,5 @@
 package team.twelve.ahu.domain.feed.presentation.controller
 
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
