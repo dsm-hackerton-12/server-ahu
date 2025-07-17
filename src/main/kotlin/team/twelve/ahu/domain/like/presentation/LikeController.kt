7962b71c9fd05273c55ace454c4f06cca84a94b9
imhanseong <hans_hanseong@dsm.hs.kr>
2025-07-17 09:32:09 +0900

    feat :: like swagger 추가

--- src/main/kotlin/team/twelve/ahu/domain/like/presentation/LikeController.kt
+++ src/main/kotlin/team/twelve/ahu/domain/like/presentation/LikeController.kt
@@ -1,5 +1,8 @@
 package team.twelve.ahu.domain.like.presentation
 
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import team.twelve.ahu.domain.like.presentation.dto.LikeResponse
@@ -12,6 +15,16 @@ import java.util.*
 class LikeController(
     private val likeService: LikeService
 ) {
+
+    @Operation(
+        summary = "피드 좋아요 등록 또는 취소",
+        description = "해당 피드에 대해 좋아요를 등록하거나 취소합니다. (토글 방식)",
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "좋아요 성공"),
+        ]
+    )
 
     @PostMapping("/{feedId}")
     fun toggleLike(
