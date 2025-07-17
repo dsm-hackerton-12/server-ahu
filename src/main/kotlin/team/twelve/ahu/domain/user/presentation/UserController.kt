cd8ebe4b5797256e1aadb3e9704f981c7f4492bd
kangeunchan <kangeunchan080310@gmail.com>
2025-07-17 09:26:33 +0900

    feat: add endpoint to retrieve user's feeds and enhance Swagger documentation in UserController

--- src/main/kotlin/team/twelve/ahu/domain/user/presentation/UserController.kt
+++ src/main/kotlin/team/twelve/ahu/domain/user/presentation/UserController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
@@ -29,19 +30,30 @@ class UserController(
 
     @PatchMapping("/nickname")
     @Operation(summary = "사용자 닉네임 변경", description = "사용자의 닉네임을 변경합니다.")
-    @ApiResponses(value = [
-        ApiResponse(responseCode = "200", description = "성공적으로 닉네임 변경"),
-        ApiResponse(responseCode = "400", description = "잘못된 요청"),
-        ApiResponse(responseCode = "401", description = "인증 실패")
-    ])
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "성공적으로 닉네임 변경"),
+            ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            ApiResponse(responseCode = "401", description = "인증 실패")
+        ]
+    )
     fun updateName(
         @RequestBody request: UpdateNameRequest,
         authentication: Authentication
-    ){
+    ) {
         val userId = UUID.fromString(authentication.name)
         updateNameService.updateName(userId, request.name)
     }
 
+    @GetMapping
+    @Operation(summary = "사용자의 모든 피드 조회", description = "자신의 모든 피드를 조회합니다.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "성공적으로 조회됨"),
+            ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            ApiResponse(responseCode = "401", description = "인증 실패")
+        ]
+    )
     fun readMyFeed(@RequestHeader("Authorization") token: String): MyFeedResponse {
         return readMyFeedService.readMyFeed(token)
     }
